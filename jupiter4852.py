7e1cd94fe3cdb2f13cd15177593e3ad476ad956f
nemoone <[omitted]>
2024-01-03 13:25:18 +0100

    Fixes of Error Corection

--- jupiter4852.py
+++ jupiter4852.py
@@ -1,7 +1,7 @@
 import warnings
 import serial
 from modbus_crc import add_crc
-
+import time
 
 
 class Jupiter:
@@ -42,20 +42,23 @@ class Jupiter:
     # Vor: temperature in °C übergeben
     # Erg: Jupiter hat jetzt die Zieltemperatur temperature
     def setTemperature(self, temperature):
-        temperature = int(round(temperature*10,1)) #Zahl mit Komma zu Zahl ohne Komma, z.B. 32.18 (°C) zu 321 (°C)
-        binaryTemperature = int(hex(temperature)[2:],16).to_bytes(2, 'big') # temperature wird zu hexstring umgewandelt; "x0" wird vom hexstring entfernt; hexstring wird als int interpretiert mit base 16; und wird zu binary umgewandelt
+        temperatureFormated = int(round(temperature*10,1)) #Zahl mit Komma zu Zahl ohne Komma, z.B. 32.18 (°C) zu 321 (°C)
+        binaryTemperature = int(hex(temperatureFormated)[2:],16).to_bytes(2, 'big') # temperature wird zu hexstring umgewandelt; "x0" wird vom hexstring entfernt; hexstring wird als int interpretiert mit base 16; und wird zu binary umgewandelt
         command = b"\x02\x06\x00\x02" + binaryTemperature
         res = self.sendAndReadCommand(command) #command wird gesendet und aufgefangen um
 
         # verify that the temperature was changed to desiered temperature. raises error if not.
-        if temperature != int(round(self.readTargetTemperature()*10,1)):
+        if temperatureFormated != int(round(self.readTargetTemperature()*10,1)):
             if self.retrys >= 3:
-                raise Exception(f"Temperature could not be set. ({retrys} retrys).\nTemperature send:{temperature}\nTemperature recived:{int(round(self.readTargetTemperature()*10,1))}\n\n(Both should be the same 3-digit, base-10 number.)\nCheck if cabels and if settings are correct.")
+                raise Exception(f"""Temperature could not be set. ({self.retrys} retrys).
+Temperature send   :{temperatureFormated}
+Temperature recived:{int(round(self.readTargetTemperature()*10,1))}
+(Both should be the same 3-digit, base-10 number.)\nCheck if cabels and if settings are correct.""")
             else:
-                warnings.warn(f"Temperature could not be set. ({retrys} retrys).\nNew try...")
-                
-            self.setTemperature(temperature)
+                warnings.warn(f"Temperature could not be set. ({self.retrys} retrys).\nNew try...")
             self.retrys = self.retrys + 1
+            self.setTemperature(temperature)
+            
 
     # Erg: gibt die aktuelle Ist-Temperatur zurück
     def readCurrentTemperature(self):
