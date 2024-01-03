278be7eaf803b24c7527b8f7417ac300cb6cb55e
Oesterle <oesterle@ad.ikz-berlin.de>
2024-01-03 12:35:48 +0100

    Added Error Correction

--- jupiter4852.py
+++ jupiter4852.py
@@ -1,3 +1,4 @@
+import warnings
 import serial
 from modbus_crc import add_crc
 
@@ -10,6 +11,8 @@ class Jupiter:
         self.stopbits = stopbits
         self.bytesize = bytesize
         self.timeout = timeout
+
+        self.retrys = 0 # for repeating the setTemperature comand after a fail
         
         self.ser = serial.Serial(self.com,
                      bytesize = self.bytesize,
@@ -39,10 +42,20 @@ class Jupiter:
     # Vor: temperature in °C übergeben
     # Erg: Jupiter hat jetzt die Zieltemperatur temperature
     def setTemperature(self, temperature):
-        temperature = int(round(temperature*10,1)) #Zahl mit Komma zu Zahl ohne Komma, z.B. 32.1 (°C) zu 321 (°C)
+        temperature = int(round(temperature*10,1)) #Zahl mit Komma zu Zahl ohne Komma, z.B. 32.18 (°C) zu 321 (°C)
         binaryTemperature = int(hex(temperature)[2:],16).to_bytes(2, 'big') # temperature wird zu hexstring umgewandelt; "x0" wird vom hexstring entfernt; hexstring wird als int interpretiert mit base 16; und wird zu binary umgewandelt
         command = b"\x02\x06\x00\x02" + binaryTemperature
-        res = self.sendAndReadCommand(command) #comand wird gesendet und aufgefangen um 
+        res = self.sendAndReadCommand(command) #command wird gesendet und aufgefangen um
+
+        # verify that the temperature was changed to desiered temperature. raises error if not.
+        if temperature != int(round(self.readTargetTemperature()*10,1)):
+            if self.retrys >= 3:
+                raise Exception(f"Temperature could not be set. ({retrys} retrys).\nTemperature send:{temperature}\nTemperature recived:{int(round(self.readTargetTemperature()*10,1))}\n\n(Both should be the same 3-digit, base-10 number.)\nCheck if cabels and if settings are correct.")
+            else:
+                warnings.warn(f"Temperature could not be set. ({retrys} retrys).\nNew try...")
+                
+            self.setTemperature(temperature)
+            self.retrys = self.retrys + 1
 
     # Erg: gibt die aktuelle Ist-Temperatur zurück
     def readCurrentTemperature(self):
