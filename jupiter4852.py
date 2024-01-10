dd63b0b9915aad88b373a1aa533917f104bd939c
Felix-Oesterle-IKZ <141313263+Felix-Oesterle-IKZ@users.noreply.github.com>
2024-01-10 15:31:49 +0100

    Update jupiter4852.py
    more comments

--- jupiter4852.py
+++ jupiter4852.py
@@ -25,34 +25,37 @@ class Jupiter:
         
         
         
-    # Gibt denn command mit Cycle Redunency Check (CRC) zurück. 
+    # Returns command string with Cycle Redunency Check (CRC) string at end
     def addCRC(self, command):
         signedCommand = add_crc(command)
         return signedCommand
     
-    #sendet einen Befehl an das Gerät und gibt die Antwort zurück
+    # send a command to the Jupiter
+    # returns the answer from the Jupiter.
     def sendAndReadCommand(self, command):
         signedCommand = self.addCRC(command)
         self.ser.write(signedCommand)
         response1 = self.ser.readline()
-        response2 = self.ser.readline() # falls ein "/n" in der Antwort vorkommt wird trozdem alles gelesen
+        response2 = self.ser.readline() # catch the whole message incase of a "\n" character. this is bad coding, the better soultion would be to calculate the byte length of the response and then read that abound of bytes
         return response1 + response2
 
 
-    # Vor: temperature in °C übergeben
-    # Erg: Jupiter hat jetzt die Zieltemperatur temperature
+    # input: temperature in °C
+    # result: Jupiter will now have the input temperature
     def setTemperature(self, temperature):
-        temperatureFormated = int(round(temperature*10,1)) #Zahl mit Komma zu Zahl ohne Komma, z.B. 32.18 (°C) zu 321 (°C)
-        binaryTemperature = int(hex(temperatureFormated)[2:],16).to_bytes(2, 'big') # temperature wird zu hexstring umgewandelt; "x0" wird vom hexstring entfernt; hexstring wird als int interpretiert mit base 16; und wird zu binary umgewandelt
+        temperatureFormated = int(round(temperature*10,1)) # format number, e.g. 32.18 (°C) to 321 (°C)
+        
+        # temperature is converted to hexstring; "x0" is removed from hexstring; hexstring is interpret as int with base 16; and is converted to binary. easy.
+        binaryTemperature = int(hex(temperatureFormated)[2:],16).to_bytes(2, 'big') 
         command = b"\x02\x06\x00\x02" + binaryTemperature
-        res = self.sendAndReadCommand(command) #command wird gesendet und aufgefangen um
+        res = self.sendAndReadCommand(command) # command is send and response is catched to avoid misinterpretation
 
         # verify that the temperature was changed to desiered temperature. raises error if not.
-        if temperatureFormated != int(round(self.readTargetTemperature()*10,1)):
+        if temperatureFormated != int(round(self.readSetpointTemperature()*10,1)):
             if self.retrys >= 3:
                 raise Exception(f"""Temperature could not be set. ({self.retrys} retrys).
 Temperature send   :{temperatureFormated}
-Temperature recived:{int(round(self.readTargetTemperature()*10,1))}
+Temperature recived:{int(round(self.readSetpointTemperature()*10,1))}
 (Both should be the same 3-digit, base-10 number.)\nCheck if cabels and if settings are correct.""")
             else:
                 warnings.warn(f"Temperature could not be set. ({self.retrys} retrys).\nNew try...")
@@ -60,16 +63,16 @@ Temperature recived:{int(round(self.readTargetTemperature()*10,1))}
             self.setTemperature(temperature)
             
 
-    # Erg: gibt die aktuelle Ist-Temperatur zurück
+    # returns current Temperature of Jupiter in °C
     def readCurrentTemperature(self):
         command = b"\x02\x03\x00\x01\x00\x02\x95\xF8"
         res = self.sendAndReadCommand(command)
         tCurrent = int.from_bytes(res[res.find(b"\x04")+1:-4], "big")/10
         return tCurrent
     
-    # Erg: gibt die aktuelle Soll-Temperatur zurück
-    def readTargetTemperature(self):
+    # returns desiered Temperature / Setpoint (SP) of Jupiter in °C
+    def readSetpointTemperature(self):
         command = b"\x02\x03\x00\x01\x00\x02\x95\xF8"
         res = self.sendAndReadCommand(command)
-        tTarget  = int.from_bytes(res[res.find(b"\x04")+3:-2], "big")/10
-        return tTarget
+        SP  = int.from_bytes(res[res.find(b"\x04")+3:-2], "big")/10
+        return SP
